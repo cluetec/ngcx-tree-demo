36f6d090aa9a02a572421959063344436258be63
Michael Niedermaier <mn@cluetec.de>
2024-03-04 13:08:30 +0100

    correct ref

--- src/app/samples/custom-node-template.ts
+++ src/app/samples/custom-node-template.ts
@@ -4,7 +4,7 @@ import { someFruitNodes } from './model/nodes';
 export const customNodeTemplate: NgcxTreeSampleData = {
   title: 'Custom angular template',
   description: `Use your own angular template to render each node. <br> 
-  See Html-Template <a href="https://github.com/cluetec/ngcx-tree-demo/blob/main/src/app/app.component.html#L41">here</a><br> 
+  See Html-Template <a href="https://github.com/cluetec/ngcx-tree-demo/blob/main/src/app/app.component.html#L48">here</a><br> 
   See Angular code <a href="https://github.com/cluetec/ngcx-tree-demo/blob/main/src/app/app.component.ts">here</a>`,
   nodes: someFruitNodes,
   config: {},
