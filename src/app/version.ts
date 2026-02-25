eac1934bfeca2b78cde6b359ecfd8b42a9da5562
Michael Niedermaier <mn@cluetec.de>
2026-02-25 14:37:49 +0100

    update to angular 21

--- src/app/version.ts
+++ src/app/version.ts
@@ -3,7 +3,7 @@
    * Do not touch - Generated file.
    **/
   export class Versions {
-    public static readonly appVersion = '20.1.0';
-    public static readonly ngcxTreeVersion = '20.1.0';
+    public static readonly appVersion = '21.0.0';
+    public static readonly ngcxTreeVersion = '21.0.0';
   }
   
